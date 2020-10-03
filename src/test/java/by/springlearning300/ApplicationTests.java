08cb730075c5987a48723dccc61656e7e136469c
EugeneYaroshevich <evgeniy_yaroshevich@mail.ru>
2020-10-03 16:00:52 +0300

    Init commit

---
+++ src/test/java/by/springlearning300/ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.springlearning300;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
